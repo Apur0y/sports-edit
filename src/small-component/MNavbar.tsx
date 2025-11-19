002813a2f3af5b48267db44431dc6f8347a83dca
Apur0y <apuroy2785@gmail.com>
2025-11-19 23:15:52 +0600

    new start

--- src/small-component/MNavbar.tsx
+++ src/small-component/MNavbar.tsx
@@ -7,7 +7,7 @@ export default function MNavbar() {
   return (
     <nav className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 z-50 md:hidden">
       <div className="flex justify-between items-center px-4 py-2">
-        {/* Home */}
+        {/* Homes */}
         <NavItem icon={<AiFillHome size={24} />} label="Home" />
         
         {/* My Network */}
