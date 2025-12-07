c066555f9b5da3a57dae8306b6dfb257d3e78281
Apur0y <apuroy2785@gmail.com>
2025-12-08 02:10:25 +0600

    Update the about us section

--- src/conponents/home/AboutUs.tsx
+++ src/conponents/home/AboutUs.tsx
@@ -18,7 +18,7 @@ const owners: Owner[] = [
     name: 'Apu Roy',
     title: 'Video Editor',
     message:
-      "Technology drives everything we do, but it's the people who make it matter. We’re building something meaningful — together.",
+      "Technology drives everything we do, but it's the people who make it matter. We’re building something meaningful — together. Make it for you and belive yourself.",
     imageUrl: '/apu-roy.png',
   },
 ];
@@ -28,12 +28,14 @@ export default function AboutUs() {
   return (
     <div className=" bg-fixed bg-center bg-cover bg-no-repeat" style={{ backgroundImage: "url('/multisports.png')" }}>
       <div className="h-full w-full flex items-center justify-center bg-black/70 text-white pt-32">
-          <div className="max-w-6xl mx-auto text-center text-white">
+          <div className="max-w-6xl mx-auto text-white">
+            
           <div className="grid gap-10 md:grid-cols-2">
             {owners.map((owner, index) => (
-              <div key={index} className="bg-primary/20 bg-opacity-10 p-6 rounded-lg shadow-lg backdrop-blur-md">
-                <div className="flex flex-col items-center">
-                  <div className="w-24 h-24 relative rounded-full overflow-hidden mb-4">
+              <div key={index} >
+                <p className="bg-white text-black bg-opacity-10 p-6  shadow-lg backdrop-blur-md">{owner.message}</p>
+                <div className="flex flex-row-reverse  items-center m-4 gap-2">
+                  <div className="w-24 h-24 relative bg-white p-1.5 overflow-hidden mb-4">
                     <Image
                       src={owner.imageUrl}
                       alt={owner.name}
@@ -42,9 +44,12 @@ export default function AboutUs() {
                       
                     />
                   </div>
+                  <div className=''>
+
                   <h3 className="text-xl font-semibold">{owner.name}</h3>
                   <p className="text-sm  italic mb-4">{owner.title}</p>
-                  <p className="">{owner.message}</p>
+                  </div>
+        
                 </div>
               </div>
             ))}
